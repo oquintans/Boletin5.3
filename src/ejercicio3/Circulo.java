797472131555097807e362d6bbb7f03ab3515c13
oquintansocampo <oquintansocampo@Eniac28.danielcastelao.org>
2014-10-16 12:20:48 +0200

    finish

--- src/ejercicio3/Circulo.java
+++ src/ejercicio3/Circulo.java
@@ -2,11 +2,31 @@
 
 
 package ejercicio3;
-
-/**
- *
- * @author oquintansocampo
- */
 public class Circulo {
-
+    private double radio;
+    private double PI = 3.14;
+    
+    public Circulo () {
+        
+    }
+    
+    public Circulo (double r, double p) {
+        radio = r;
+        PI = p;        
+    }
+    
+    public double getRadio (){
+        return radio;
+    }
+    public void setRadio (float r) {
+        radio=r;
+    }
+    public double calcularArea (){
+       double area = (Math.pow(radio,2))*PI;
+       return area;   
+    }
+    public double calcularLongitud () {
+        double longitud = 2*radio*PI;
+        return longitud;
+    }
 }
--- src/ejercicio3/Ejercicio3.java
+++ src/ejercicio3/Ejercicio3.java
@@ -1,16 +1,4 @@
-/*
-3- Implementa unha aplicación que nos permita traballar con circulos e circunferencias .Para eso crea a clase Circulo cas seguintes características :
 
-atributos : radio ( tipo double )
-Pi ( constante de tipo double e valor 3.14 )
-
-métodos : *   constructores, sen e con parámetros
-                 *  métodos de acceso
-                 *   calcularArea : Non recibe nada e devolve o valor da área . Para calcular radio ^ 2 utiliza a clase Math e o método pow (….)
-                 *  calcularLonxitude : Non recibe nada e devolve o valor da lonxitude da circunferencias
-   
-     Crea unha clase principal para comprobar que todo funciona ben
- */
 package ejercicio3;
 
 /**
@@ -23,7 +11,10 @@ public class Ejercicio3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Circulo parida1=new Circulo();
+        parida1.setRadio(1);
+        System.out.println("Longitud= "+parida1.calcularLongitud());
+        System.out.println("Area= "+parida1.calcularArea());
     }
     
 }
